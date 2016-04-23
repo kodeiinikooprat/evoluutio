9ea51d8d509ccf797e68436e845200bc980a57f8
Ville Vaara <villepvaara@gmail.com>
2016-04-23 10:28:44 +0300

    world - näkymän ja species- näkymän alkua. EvolutionController alkaa olla aika sotkua, aika siivota pian

--- src/main/java/com/kodeiinia/EvolutionController.java
+++ src/main/java/com/kodeiinia/EvolutionController.java
@@ -7,6 +7,9 @@ import com.kodeiinia.database.WorldDbService;
 import com.kodeiinia.database.WorldMongoDao;
 import com.kodeiinia.gamelogic.Species;
 import com.mongodb.DB;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.Map;
 import spark.ModelAndView;
@@ -29,36 +32,47 @@ public class EvolutionController {
     }
 
     private void setupEndpoints() {
-        get(new FreeMarkerRoute("/") {
+
+        get(new Route("/") {
             @Override
-            public ModelAndView handle(Request request, Response response) {
-                Map<String, Object> viewObjects = new HashMap();
-                World world = worldDbService.readOne(1);
-
-                if (world != null) {
-                    viewObjects.put("world", world);
-
-                } else {
-                    viewObjects.put("noWorlds", "No worlds present in database!");
-                }
-
-                Species species = speciesDbService.readOne(1);
-                if (species != null) {
-                    viewObjects.put("species", species);
-                } else {
-                    viewObjects.put("noSpecies", "No species present in database!");
-                }
-
-                viewObjects.put("sidebar", "sidebar_speciesCreator.ftl");
-                viewObjects.put("primary", "world.ftl");
-                return modelAndView(viewObjects, "layout.ftl");
+            public Object handle(Request request, Response response) {
+                response.status(301);
+                response.redirect("/world/1");
+                return "";
             }
         });
 
-        get(new FreeMarkerRoute("/nextturn") {
+//        get(new FreeMarkerRoute("/") {
+//            @Override
+//            public ModelAndView handle(Request request, Response response) {
+//                Map<String, Object> viewObjects = new HashMap();
+//                World world = worldDbService.readOne(1);
+//
+//                if (world != null) {
+//                    viewObjects.put("world", world);
+//
+//                } else {
+//                    viewObjects.put("noWorlds",
+//                            "No worlds present in database!");
+//                }
+//
+//                Species species = speciesDbService.readOne(1);
+//                if (species != null) {
+//                    viewObjects.put("species", species);
+//                } else {
+//                    viewObjects.put("noSpecies",
+//                            "No species present in database!");
+//                }
+//
+//                viewObjects.put("sidebar", "sidebar_speciesCreator.ftl");
+//                viewObjects.put("primary", "world.ftl");
+//                return modelAndView(viewObjects, "layout.ftl");
+//            }
+//        });
+        get(new /*FreeMarker*/Route("/nextturn") {
             @Override
-            public ModelAndView handle(Request request, Response response) {
-                Map<String, Object> viewObjects = new HashMap();
+            public Object /*ModelAndView*/ handle(Request request, Response response) {
+//                Map<String, Object> viewObjects = new HashMap();
                 World world = worldDbService.readOne(1);
                 Species species = speciesDbService.readOne(1);
                 world.increaseTurn();
@@ -73,14 +87,19 @@ public class EvolutionController {
                         species.getName(),
                         newAmount);
 
-                viewObjects.put("world", world);
-                viewObjects.put("species", species);
+//                viewObjects.put("world", world);
+//                viewObjects.put("species", species);
+//
+//                viewObjects.put("sidebar", "sidebar_speciesCreator.ftl");
+//                viewObjects.put("primary", "world.ftl");
 
-                viewObjects.put("sidebar", "sidebar_speciesCreator.ftl");
-                viewObjects.put("primary", "world.ftl");
+//                return modelAndView(viewObjects, "layout.ftl");
 
-                return modelAndView(viewObjects, "layout.ftl");
+                response.status(301);
+                response.redirect("/");
+                return "";
 
+                
             }
         });
 
@@ -92,13 +111,74 @@ public class EvolutionController {
                 String name = request.queryParams("species-name");
                 int numberOfAnimals = 2;
                 int worldRef = 1;
-                Species species = new Species(id, name, numberOfAnimals, worldRef);
+                Species species = new Species(
+                        id, name, numberOfAnimals, worldRef);
                 speciesDbService.create(species);
                 response.status(201);
                 response.redirect("/");
                 return "";
             }
         });
+
+        get(new FreeMarkerRoute("/world/:worldId/species/:speciesId") {
+            @Override
+            public ModelAndView handle(Request request, Response response) {
+                Integer worldId = Integer.parseInt(
+                        request.params(":world"));
+                Integer speciesId = Integer.parseInt(
+                        request.params(":speciesId"));
+
+                Map<String, Object> viewObjects = new HashMap();
+                World world = worldDbService.readOne(worldId);
+                Species species = speciesDbService.readOne(speciesId);
+
+                viewObjects.put("sidebar", "sidebar_speciesCreator.ftl");
+                viewObjects.put("primary", "world.ftl");
+                return modelAndView(viewObjects, "layout.ftl");
+            }
+
+        });
+
+        get(new FreeMarkerRoute("/world/:worldId") {
+            @Override
+            public ModelAndView handle(Request request, Response response) {
+                Integer worldId = Integer.parseInt(
+                        request.params(":worldId"));
+
+                Map<String, Object> viewObjects = new HashMap();
+                World world = worldDbService.readOne(worldId);
+                ArrayList<Species> speciesList = speciesDbService.readAll();
+
+                if (world != null) {
+                    viewObjects.put("world", world);
+
+                    if (speciesList.isEmpty()) {
+                        String message = "The world \""
+                                + world.getTitle()
+                                + "\" is bereft of life!";
+                        viewObjects.put("noSpecies", message);
+                    } else {
+                        Deque<Species> showSpecies = new ArrayDeque<>();
+
+                        for (Species species : speciesList) {
+                            showSpecies.addFirst(species);
+                        }
+
+                        viewObjects.put("speciesList", showSpecies);
+                    }
+
+                } else {
+                    viewObjects.put("noWorlds",
+                            "The universe is empty and sad!");
+                }
+
+                viewObjects.put("sidebar", "sidebar_speciesCreator.ftl");
+                viewObjects.put("primary", "world.ftl");
+                return modelAndView(viewObjects, "layout.ftl");
+            }
+
+        });
+
     }
 
 }
--- target/classes/spark/template/freemarker/world.ftl
+++ target/classes/spark/template/freemarker/world.ftl
@@ -4,12 +4,15 @@
 <#else>
     <h3>${world.getTitle()}</h3>     
     <h3>${world.getTurn()}</h3>      
+    <#if noSpecies??>
+        <h3>${noSpecies}</h3>
+    <#else>
+        <div class="starter-template">
+            <#list speciesList as species>
+            <h3>${species.getName()}</h3>
+            <h4>${species.getNumberOfAnimals()}</h4>
+            </#list>
+            </div>
+    </#if>
 </#if>
-<#if noSpecies??>
-    <h3>${noSpecies}</h3>
-<#else>
-    <h3>${species.getName()}</h3>     
-    <h3>${species.getNumberOfAnimals()}</h3>      
-</#if>
-    
-    </div>
+</div>
